f919a220e6de7133710115ac794ba7550fff467a
markdan58 <mark.dan@strathmore.edu>
2018-11-26 12:11:38 +0300

    added manager,app,migrate

--- manage.py
+++ manage.py
@@ -5,3 +5,10 @@ from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
