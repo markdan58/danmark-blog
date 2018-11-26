c98851f51517dfde60fa138826ce887792cadd41
markdan58 <mark.dan@strathmore.edu>
2018-11-26 12:08:54 +0300

    created a class Testconfig

--- config.py
+++ config.py
@@ -26,3 +26,6 @@ class ProdConfig(Config):
    
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://danmark:5898danmark@localhost/blog'
