29c9baa34dd4361992a1a65210465406cee029fa
markdan58 <mark.dan@strathmore.edu>
2018-11-26 12:12:01 +0300

    added managershell

--- manage.py
+++ manage.py
@@ -12,3 +12,17 @@ manager.add_command('server',Server)
 
 migrate = Migrate(app,db)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+
+
+
+
+if __name__ == '__main__':
+    manager.run()
+
+
+
