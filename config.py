6fb50d4d892138dffbfd952cb806df7a3cb29e46
markdan58 <mark.dan@strathmore.edu>
2018-11-26 12:07:33 +0300

    created a class config

---
+++ config.py
@@ -0,0 +1,15 @@
+class Config:
+    '''
+    This is the general configuration parent class
+    '''
+
+    # Email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://danmark:5898danmark@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
