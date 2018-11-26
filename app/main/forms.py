9ae8617bc70dae982020c72371ab8840a6484243
markdan58 <mark.dan@strathmore.edu>
2018-11-26 09:36:02 +0300

    created a comments form class

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,3 +4,10 @@ from ..models import User
 from wtforms import ValidationError
 from wtforms.validators import Required, Email, EqualTo
 from .. import db
+
+
+
+
+class CommentsForm(FlaskForm):
+    comments = TextAreaField('Newblog Comments', validators=[Required()])
+    submit = SubmitField('Submit')
